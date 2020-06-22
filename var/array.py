f4f1c68436a56c9dca6093e28cdff0b20f20f640
kgwny <kigawanaoya@gmail.com>
2020-06-22 23:04:36 +0900

    Add some snippets

---
+++ var/array.py
@@ -0,0 +1,8 @@
+array = [1, 2, 3]
+
+print(array)
+# [1, 2, 3]
+
+# 内包表記
+print([x * 2 for x in array])
+# [2, 4, 6]
---
+++ var/map.py
@@ -0,0 +1,9 @@
+i = '1 2 3'
+
+print(map(int, i.split(' ')))
+
+print(list(map(int, i.split(' '))))
+# [1, 2, 3]
+
+print([int(x) for x in i.split(' ')])
+# [1, 2, 3]
