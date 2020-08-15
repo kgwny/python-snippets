335a8102f2f34075ed966609a866c8508259920f
kgwny <kigawanaoya@gmail.com>
2020-08-15 22:44:10 +0900

    add snippet

---
+++ basic/url_result.py
@@ -0,0 +1,19 @@
+#!/usr/local/bin/python
+
+# change above line to point to local python executable
+
+import urllib.request
+
+# create URL with desired search parameters
+
+url = "http://example.com?param=hoge"
+
+print(url)
+
+# retrieve URL and  write results to filename
+
+filename = "result.txt"
+
+urllib.request.urlretrieve(url, filename)
+
+### Done!
