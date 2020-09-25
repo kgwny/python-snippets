bf5dee1d6ef3975b76fff84d3444eef513d27ab2
kgwny <kigawanaoya@gmail.com>
2020-09-26 08:28:02 +0900

    update snippet

---
+++ class/function_class.py
@@ -0,0 +1,10 @@
+
+class some_class:
+    def __init__(self, something):
+        self.something = something
+
+    def some_function(self):
+        print(self.something)
+
+a = some_class('some_value')
+a.some_function()
--- technic/file_inout.py
+++ technic/file_inout.py
@@ -1,11 +1,12 @@
 
-out_list = [1, 2, 3, 4, 5]
+LINE_FEED_CODE = '\n'
 
+out_list = [1, 2, 3, 4, 5]
 file_path = 'tmp.txt'
 
 # リストから取得した値に改行コードを付けてファイルに書き込む
 with open(file_path, mode='w') as f:
-    [f.write(str(s) + '\n') for s in out_list]
+    [f.write(str(s) + LINE_FEED_CODE) for s in out_list]
 
 # ファイルからリストを取り出してリスト内の全要素に付与された改行コードを削る
 with open(file_path) as f:
