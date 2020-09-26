995816ff16b7fa92f5b02277408f365a25c66709
kgwny <kigawanaoya@gmail.com>
2020-09-26 20:14:09 +0900

    add class example

---
+++ class/example.py
@@ -0,0 +1,14 @@
+class Example():
+
+    # __init__はコンストラクタ
+    def __init__(self, a, b, c):
+        self.num1 = a
+        self.num2 = b
+        self.num3 = c
+
+    def print_sum(self):
+        sum = self.num1 + self.num2 + self.num3
+        print(sum)
+
+myinstance = Example(1, 2, 3)
+myinstance.print_sum()
