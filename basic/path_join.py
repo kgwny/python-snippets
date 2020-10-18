446c4aad6b3d782fddfbd28a92b83466dcaeb7c7
kgwny <kigawanaoya@gmail.com>
2020-10-18 23:02:49 +0900

    add path_join

---
+++ basic/path_join.py
@@ -0,0 +1,10 @@
+import os.path
+
+base_path = '/Users/admin'
+python_code_path = 'Documents/python/practise.py'
+
+# パスを連結する
+path = os.path.join(base_path, python_code_path)
+
+print(path)
+# /Users/admin/Documents/python/practise.py
