f1d1eeabc0a49857b0024608dc99a662418e07f8
kgwny <kigawanaoya@gmail.com>
2020-10-22 21:34:11 +0900

    add list snippet

---
+++ list/list_etc.py
@@ -0,0 +1,12 @@
+string = 'Hello World'
+
+print(string.split())
+# ['Hello', 'World']
+
+array = list(string)
+print(array)
+# ['H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd']
+
+arr_str = ''.join(array)
+print(arr_str)
+# Hello World
