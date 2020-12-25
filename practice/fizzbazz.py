a0c8cdb6c1f02284583630e800015db30a1aa402
kgwny <kigawanaoya@gmail.com>
2020-12-25 22:09:38 +0900

    add fizzbazz snippet

---
+++ practice/fizzbazz.py
@@ -0,0 +1,12 @@
+numbers = [45, 22, 14, 65, 97, 72]
+
+for i in range(len(numbers)):
+    if numbers[i] % 3 == 0 and numbers[i] % 5 == 0:
+        numbers[i] = 'fizzbuzz'
+    elif numbers[i] % 3 == 0:
+        numbers[i] = 'fizz'
+    elif numbers[i] % 5 == 0:
+        numbers[i] = 'buzz'
+
+print(numbers)
+# ['fizzbuzz', 22, 14, 'buzz', 97, 'fizz']
