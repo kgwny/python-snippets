474af5bf76745ea30b59330f3a2fe645e36c58a6
kgwny <kigawanaoya@gmail.com>
2021-02-04 23:46:15 +0900

    add tuple snippet

---
+++ tuple/tuple2.py
@@ -0,0 +1,22 @@
+# 空のタプル
+empty_tuple = ()
+print(type(empty_tuple))
+# <class 'tuple'>
+
+# 要素がひとつだけのタプル
+one_element_tuple = ('something')
+print(one_element_tuple)
+print(type(one_element_tuple))
+# <class 'str'>
+# 文字列になってしまう問題がある
+
+# 回避策
+# 書式その１
+one_element_tuple = ('something',)
+print(one_element_tuple)
+# ('something',)
+
+# 書式その２
+one_element_tuple = 'something',
+print(one_element_tuple)
+# ('something',)
